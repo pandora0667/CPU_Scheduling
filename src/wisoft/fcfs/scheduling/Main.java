a1bffae2f0e1c6163dc2917f70a0d6e7b7f3de70
seongwonlee <jusk2@naver.com>
2017-08-04 21:15:39 +0900

    commit first setting

---
+++ src/wisoft/fcfs/scheduling/Main.java
@@ -0,0 +1,15 @@
+package wisoft.fcfs.scheduling;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("    —------—  CPU 'First In First Out' Scheduling in JAVA  —--------");
+
+
+    // Process Information
+    // Process ID, Burst Time, Arrival Time
+
+    // GANTT CHART
+    // Average Time
+    // Average Turn Around Time
+  }
+}
